0d2710dbd9a9ae7bd7a6e219dfc8e983d04377ae
parulgangwal2908 <parul.gangwal@innogent.in>
2025-10-29 21:09:08 +0530

    Updated files

---
+++ Day 01/factorial_recursion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public int fact(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = fact(n);
+        System.out.println(ans);
+    }
+}
